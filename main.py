a3cd66448f238796110d4f7aeab080dd0a02443b
TopShark123 <vyotov07@gmail.com>
2023-05-11 10:41:12 +0300

    changes

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pygame
 import os
+from sys import exit
 import enemies,hero,map
 
-print(5)
-print(6)
-print(7)
-print(8)
+HEIGHT, WIGHT = 400, 800
+
+pygame.init()
+screen = pygame.display.set_mode((WIGHT,HEIGHT))
+pygame.display.set_caption('pygame project')
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
