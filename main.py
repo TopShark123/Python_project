70c3a4fce488ff5e740cd7aded3c367d447dc34a
Kristian Nikolov <kristian.b.nikolov.2021@elsys-bg.org>
2023-05-11 22:38:41 +0300

    importing the images

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pygame
 import os
+from sys import exit
+from pytmx import load_pygame
 import enemies,hero,map
 
-print(5)
-print(6)
-print(7)
-print(8)
+HEIGHT, WIGHT = 400, 800
+
+pygame.init()
+screen = pygame.display.set_mode((WIGHT,HEIGHT))
+pygame.display.set_caption('pygame project')
+clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock.tick(60)
